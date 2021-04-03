fb3daa42811130c23ca8a5ec6dcdd61181a96b51
SHUNJI1018 <hide971gg@gmail.com>
2021-04-03 05:36:29 +0000

    [finish] Bookers2 debug

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,58 @@
+class BooksController < ApplicationController
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def index
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+        render "edit"
+    else
+        redirect_to books_path
+    end
+  end
+
+
+
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destoy
+    redirect_to books_path, notice: "Book was successfully destroyed."
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title)
+  end
+
+end
